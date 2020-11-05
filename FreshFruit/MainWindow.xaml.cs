69768d6124447e9435be4d0f49b254c0a5787d62
alfarid04 <alfarid.yusuf@students.amikom.ac.id>
2020-11-05 12:36:20 +0700

    Add project files.

---
+++ FreshFruit/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using FreshFruit.Controller;
+using FreshFruit.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FreshFruit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, BucketEventListener
+    {
+        Seller Alfa;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Bucket keranjangBuah = new Bucket(4);
+            BucketController bucketController = new BucketController(keranjangBuah, this);
+
+            Alfa = new Seller("toni", bucketController);
+
+            ListBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+        public void onFailed(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public void onSucceed(string message)
+        {
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddAnggurClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit anggur = new Fruit("Anggur");
+            Alfa.addFruit(anggur);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddAppleClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit Apple = new Fruit("Apple");
+            Alfa.addFruit(Apple);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddBananaClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit Banana = new Fruit("Banana");
+            Alfa.addFruit(Banana);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddOrangeClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit orange = new Fruit("orange");
+            Alfa.addFruit(orange);
+
+            ListBoxBucket.Items.Refresh();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,57 @@
+﻿# FRESH FRUIT
+Fresh Fruit merupakan sebuah aplikasi yang digunakan untuk menambahkan buah-buahan kedalam keranjang, serta menampilkan gambar/icon . Pada Aplikasi ini juga terdapat konsep MVC (Model View Controller).
+
+# SCOPE & FUNCTIONALITIES
+- User dapat menekan tombol Add untuk menambahkan buah/fruit
+- User dapat menampilkan nama buah yang sudah di ADD kedalam keranjang (Listbox)
+- User dapat menghapus nama uah yang sudah tampil dikeranjang (Listbox) dengan menekan tombol DELETE.
+# HOW DOES IT WORKS?
+Pada Method Fruit.cs itu merupakan Logic View yang digunakan untuk menampilkan nama buah pada listbox. berikut merupakan source codenya
+```csharp
+ public string name { get; set; }
+
+        public Fruit(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+```
+logic model bisnis pada MainWindow.xaml.cs digunakan untuk menambahkan buah dengan cara menekan tombol ADD pada setiap gambar , dan menampilkannya pada listbox melalui method Fruit.cs. berikut merupakan source code pada MainWindow.xaml.cs
+```csharp
+
+        private void OnButtonAddAnggurClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit anggur = new Fruit("Anggur");
+            Alfa.addFruit(anggur);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddAppleClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit Apple = new Fruit("Apple");
+            Alfa.addFruit(Apple);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddBananaClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit Banana = new Fruit("Banana");
+            Alfa.addFruit(Banana);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddOrangeClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit orange = new Fruit("orange");
+            Alfa.addFruit(orange);
+
+            ListBoxBucket.Items.Refresh();
+        }
+```
+      
